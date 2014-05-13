d206c0367168fa4d3e5e1b028120fb5ab0460348
IUnknown68 <arne@salsitasoft.com>
2014-05-13 14:49:56 +0200

    Adjusted generated files paths to make sure project builds standalone. Adjusted targetspecs/targetver.h
    Signed-off-by: IUnknown68 <arne@salsitasoft.com>

--- code/targetspecs/targetver.h
+++ code/targetspecs/targetver.h
@@ -8,11 +8,11 @@
 #include <WinSDKVer.h>
 
 #ifndef _WIN32_WINNT
-# define _WIN32_WINNT  0x0501
+# define _WIN32_WINNT  0x0600
 #endif
 
 #ifndef _WIN32_WINNT
-# define _WIN32_IE     0x0800
+# define _WIN32_IE     0x0900
 #endif
 
 #include <SDKDDKVer.h>
